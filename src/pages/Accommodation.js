7a5cc7c7cb5bd7b0b2210f51884dc23628143005
Anthony <anthony.rabot@gmail.com>
2022-12-12 23:43:19 +0100

    WIP Accommodation

--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Header from '../components/Header'
 import { useParams } from 'react-router-dom'
 import { useFetch } from '../utils/Hooks'
+import Loader from '../components/Loader'
+import MainTitle from '../components/MainTitle'
+import Host from '../components/Host'
+import Tag from '../components/Tag'
 
-function Accommodation () {
+function Accommodation() {
   const { id } = useParams()
   const { data, isLoading, error } = useFetch(`/logements.json`)
 
@@ -13,25 +17,42 @@ function Accommodation () {
 
   const accommodationsList = data ? data : []
   let accommodationData = {}
+  let tags = []
   if (!isLoading) {
-    accommodationData = accommodationsList.find(x => x.id === id)
-  }
+    accommodationData = accommodationsList.find(
+      (accommodation) => accommodation.id === id
+    )
+    console.log(accommodationData)
 
-  console.log(accommodationData)
+    tags = accommodationData.tags.map((tag, index) => {
+      return <Tag key={index} value={tag} />
+    })
+  }
 
   return (
     <div className="container">
       <Header />
       {isLoading ? (
-        <p>Chargement</p>
+        <Loader />
       ) : (
-        <section className="logement">
-          <h1>{accommodationData.title}</h1>
+        <article className="accommodation">
+          <header className="accommodation__header">
+            <section className="accommodation__infos">
+              <MainTitle
+                title={accommodationData.title}
+                location={accommodationData.location}
+              />
+              <div className="accommodation__tags">{tags}</div>
+            </section>
+            <section className="accommodation__host__infos">
+              <Host host={accommodationData.host} />
+            </section>
+          </header>
+
           <p>{accommodationData.id}</p>
-        </section>
+        </article>
       )}
     </div>
   )
 }
-
 export default Accommodation
