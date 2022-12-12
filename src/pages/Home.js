b2c99a20813b5669fca799e686632618a0fe1c0a
Anthony <anthony.rabot@gmail.com>
2022-12-12 18:28:13 +0100

    Add Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Header from '../components/Header'
+import { useFetch } from '../utils/Hooks'
+import Card from '../components/Card'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+  const { data, isLoading, error } = useFetch(`/logements.json`)
+  const logementsList = data ? data : []
+
+  if (error) {
+    return <span>Il y a un problème</span>
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <Header />
+      {isLoading ? (
+        <p>Chargement</p>
+      ) : (
+        <section className="logements">
+          {logementsList?.map((logement) => (
+            <Link to={`/appartement/${logement.id}`} key={`${logement.id}`}>
+              <Card title={logement.title} picture={logement.cover} />
+            </Link>
+          ))}
+        </section>
+      )}
     </div>
   )
 }
