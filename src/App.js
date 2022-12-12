f9a01102773371498e3231582a5916af1c421029
Anthony <anthony.rabot@gmail.com>
2022-12-12 18:27:24 +0100

    Add routes on App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Error from './pages/Error'
+import About from './pages/About'
+import Accommodation from './pages/Accommodation'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/a-propos" element={<About />}></Route>
+        <Route path="/appartement/:id" element={<Accommodation />} />
         <Route path="*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
