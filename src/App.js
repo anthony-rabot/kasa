571d284fa5e72568dba17da9ffb82645b16ca205
Anthony <anthony.rabot@gmail.com>
2022-12-12 09:45:54 +0100

    Add routing for home and 404 page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Error from './pages/Error'
 
 const App = () => {
-  return <div></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
