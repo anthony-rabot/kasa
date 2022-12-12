6914da8a06ebc84da1cc12b38621e8540f54dbe8
Anthony <anthony.rabot@gmail.com>
2022-12-12 18:28:01 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+import Logo from '../components/Logo'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Logo />
+      <Navigation />
+    </header>
+  )
+}
+
+export default Header
